560b009bd27be724f07af0b403ea95b80c494a40
Lalvar <Mnacakanyan Lalvar>
2021-08-28 23:49:46 +0400

    second commit

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ inputBtn.addEventListener("click", function (e) {
   }
   input.value = "";
   showItemsLeft();
-  colsFun();
 });
 input.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -131,7 +130,6 @@ navBtnContainer.addEventListener("click", (e) => {
       circleJs.forEach((btn) => {
         if (btn.classList.contains("circle-icon-s-active")) {
           btn.closest("li").style.display = "none";
-          // console.log(btn.closest("li"));
           showItemsLeft();
         } else {
           btn.closest("li").style.display = "flex";
@@ -154,61 +152,8 @@ navBtnContainer.addEventListener("click", (e) => {
   }
 });
 
-let dragSrcEl = null;
-function handleDragStart(e) {
-  dragSrcEl = this;
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/html", this.innerHTML);
-}
-
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-
-  e.dataTransfer.dropEffect = "move"; // See the section on the DataTransfer object.
-
-  return false;
-}
-
-function handleDragEnter(e) {
-  // this / e.target is the current hover target.
-  this.classList.add("over");
-}
-
-function handleDragLeave(e) {
-  this.classList.remove("over"); // this / e.target is previous target element.
-}
-
-function handleDrop(e) {
-  // this/e.target is current target element.
-  if (e.stopPropagation) {
-    e.stopPropagation(); // Stops some browsers from redirecting.
-  }
-  // Don't do anything if dropping the same column we're dragging.
-  if (dragSrcEl != this) {
-    // Set the source column's HTML to the HTML of the column we dropped on.
-    dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData("text/html");
-  }
-  return false;
-}
-
-function colsFun() {
-  const cols = document.querySelectorAll(".list-items .list-item");
-  [].forEach.call(cols, function (col) {
-    col.addEventListener("dragstart", handleDragStart, false);
-    col.addEventListener("dragenter", handleDragEnter, false);
-    col.addEventListener("dragover", handleDragOver, false);
-    col.addEventListener("dragleave", handleDragLeave, false);
-    col.addEventListener("drop", handleDrop, false);
-    col.addEventListener("dragend", handleDragEnd, false);
-  });
-  function handleDragEnd(e) {
-    // this/e.target is the source node.
-    [].forEach.call(cols, function (col) {
-      col.classList.remove("over");
-    });
-  }
-}
-colsFun();
+// Drag nad drop function
+const dragArea = document.querySelector("#todo-lists");
+new Sortable(dragArea, {
+  animation: 300,
+});
